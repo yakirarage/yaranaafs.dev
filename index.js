3a585f6cb33de19fd681a0850e8e6ce64a0e45c3
yakirarage <yaranaafs@gmail.com>
2022-11-22 15:52:11 +0100

    added link embed

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ app.use(express.static("static"))
 app.set("view engine", "ejs")
 
 app.get("/", (req,res) => {
-    res.status(200).render(__dirname + "/pages/index.ejs")
+    res.status(200).render(__dirname + "/pages/index.ejs", {
+        title: 'Home', 
+        iconLoc: '/images/mainIcon.png', 
+        desc: "The personal website of a 18 year old developer.", 
+        url: 'https://maurowalker.dev/'
+    })
 })
 
 app.listen(port, () => console.log(`app is listening to ${port}`))
